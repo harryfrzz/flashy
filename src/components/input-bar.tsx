8599f2d5bbdeb69eaefa0f27d03e83880bd7cad1
harryfrzz <harikrishnac005@gmail.com>
2025-09-24 09:29:45 +0530

    update: changes to UI elements

--- src/components/input-bar.tsx
+++ src/components/input-bar.tsx
@@ -3,8 +3,8 @@ import { Paperclip } from "lucide-react";
 export default function InputBar(){
     return(
         <div className="absolute bottom-0 left-0 right-0 flex gap-2 py-5 px-3 bg-transparent">
-            <div className="flex-1 h-10 rounded-full bg-white">
-                <input className="w-full h-full px-3 outline-none text-sm rounded-full" type="text" placeholder="Type your prompt here"/>
+            <div className="flex-1 h-10 rounded-full bg-transparent border border-[rgb(96,96,96)]">
+                <input className="w-full h-full px-4 outline-none text-sm rounded-full text-white placeholder:text-[rgb(68,68,68)]" type="text" placeholder="Type your prompt here"/>
             </div>
             <button className="bg-white w-10 h-10 rounded-full flex items-center justify-center hover:bg-gray-100">
                 <Paperclip width={20}/>
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -17,7 +17,7 @@ export default function Sidebar({ isOpen, onClose }: SidebarProps) {
                 />
             )}
             <div className={`
-                fixed top-0 left-0 p-2 gap-2 h-[600px] flex justify-between flex-col w-64 bg-gray-900 z-50 transform transition-transform duration-300 ease-in-out
+                fixed top-0 left-0 p-2 gap-2 h-[600px] flex justify-between flex-col w-64 bg-black z-50 transform transition-transform duration-300 ease-in-out
                 ${isOpen ? 'translate-x-0' : '-translate-x-full'}
             `}>
                
